5a53086d7d9ddc2d54c787b5d694b86a619d4f5a
Kyle Nitzsche <kyle.nitzsche@canonical.com>
2014-02-12 13:28:54 -0500

    add script, assets, and DOCS-README to yuidoc build known api docs

---
+++ src/Ubuntu/UnityWebApps/bindings/yuidoc-build.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+set -e
+for api in alarm-api content-hub online-accounts ; do
+    echo $api
+    cd $api/client
+    mv ../../yuidoc-theme/partials/index.handlebars index.handlebars.orig
+    cp index.handlebars ../../yuidoc-theme/partials/
+    rm -rf ./docsbuild
+    yuidoc -c yuidoc.json .
+    mv index.handlebars.orig ../../yuidoc-theme/partials/index.handlebars
+    cd ../..
+done;
+exit 0
