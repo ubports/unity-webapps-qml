ce8f199dd232bacbece08cc0f2816167007ccc52
Daniel Holbach <daniel.holbach@canonical.com>
2015-03-30 18:26:31 +0200

    run ./update-docs command, add resulting docs

---
+++ src/Ubuntu/UnityWebApps/bindings/alarm-api/client/docsbuild/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Alarm",
+        "AlarmApi"
+    ],
+    "modules": [
+        "AlarmApi"
+    ],
+    "allModules": [
+        {
+            "displayName": "AlarmApi",
+            "name": "AlarmApi",
+            "description": "AlarmApi gives access to Alarm management."
+        }
+    ]
+} };
+});
---
+++ src/Ubuntu/UnityWebApps/bindings/runtime-api/client/docsbuild/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "RuntimeApi"
+    ],
+    "modules": [
+        "RuntimeApi"
+    ],
+    "allModules": [
+        {
+            "displayName": "RuntimeApi",
+            "name": "RuntimeApi",
+            "description": "RuntimeApi gives access to the application runtime information and management."
+        }
+    ]
+} };
+});
